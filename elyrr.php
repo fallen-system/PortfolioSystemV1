2be3e6eaaf41530c714020d28ddaa8f3131dd91d
_fallen_system_ <45458901+fallen-system@users.noreply.github.com>
2024-11-17 13:26:16 +0100

    Update to 2024 naming

---
+++ elyrr.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Fallen Cy - Portefolio</title>
+    <link href="style/main.css" rel="stylesheet" type="text/css">
+</head>
+<body>
+
+</div>
+</body>
+</html>
--- index.php
+++ index.php
@@ -3,20 +3,20 @@
 
 <head>
     <meta charset="utf-8">
-    <title>Elyrr / Akane Aliss</title>
+    <title>Fallen System</title>
     <link href="style/toolbox.css" rel="stylesheet" type="text/css">
     <link href="style/home.css" rel="stylesheet" type="text/css">
 </head>
 
 <body>
     <div id="header">
-        <p>Elyrridris | Akane Aliss Cosplay</p>
+        <p>Fallen System</p>
     </div>
     <h1> Choisissez votre voie</h1>
     <div id="choix">
         <a href="page/elyrr.php" class="InOption" id="elyrr">
 
-            <h2>Elyrridris - Design graphique</h2>
+            <h2>Fallen Cy - Design graphique</h2>
 
         </a>
         <a href="page/AkaAliss.php" class="InOption" id="aliss">
@@ -24,7 +24,7 @@
         </a>
     </div>
     <div id="footer">
-        <p>Créer par <a href="https://twitter.com/elyrridris">elyrrdris</a>. tout droit réservé. ©2022 | image de fond par  <a href=https://unsplash.com/@agnis_leznins?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText>Agnis Leznins </a> sur <a href="https://unsplash.com/s/photos/tokyo-night?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash </a> </p>
+        <p>Créer par <a href="https://twitter.com/elyrridris">fallen system</a>. tout droit réservé. ©2024 | image de fond par  <a href=https://unsplash.com/@agnis_leznins?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText>Agnis Leznins </a> sur <a href="https://unsplash.com/s/photos/tokyo-night?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash </a> </p>
     </div>
 </body>
 
